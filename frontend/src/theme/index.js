2bdaff418a017ce7d4e60e7d4c5ae47eae975452
Uğur Oğuzhan Obuz <ugurobuz@gmail.com>
2024-03-08 13:55:36 +0300

    chakra style hierarchy and init

---
+++ frontend/src/theme/index.js
@@ -0,0 +1,14 @@
+// Main theme entrypoint
+
+import { extendTheme } from '@chakra-ui/react';
+import styles from './styles'
+
+const overrides = {
+    styles,
+
+    components: {
+
+    },
+}
+
+export default extendTheme(overrides)
