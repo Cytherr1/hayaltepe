b789b77ede7fc17af3c3fec78062ed5c6da8e83b
hantheemp <murat.temel@bahcesehir.edu.tr>
2024-03-12 20:47:42 +0300

    .env init

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 // Import necessary modules
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -24,15 +24,21 @@ const logRoutes = require("./routes/log/log_routes");
 var app = express();
 
 // Set up middleware
-app.use(logger('dev')); // Logger middleware
+app.use(logger("dev")); // Logger middleware
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded bodies
 app.use(cookieParser()); // Parse Cookie headers
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files
 
 // Body parser and CORS middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "http://localhost:5173"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 // Mount routes
 app.use("/admin/auth", adminAuthRoutes);
@@ -45,29 +51,29 @@ app.use("/general/product", generalProdRoutes);
 app.use("/log", logRoutes);
 
 // Set up view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // Routes for index and users
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // Catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // Error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // Set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // Render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- backend/config/db_config.js
+++ backend/config/db_config.js
@@ -1,16 +1,19 @@
 const mysql = require("mysql");
-require("dotenv").config();
+require('dotenv').config({ path: '../../.env'});
 
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
 
+console.log(process.env.DB_HOST);
+
 const getConnection = () => {
+
   return new Promise((resolve, reject) => {
     pool.getConnection((err, connection) => {
       if (err) {
@@ -23,11 +26,6 @@ const getConnection = () => {
 
 const releaseConnection = (connection) => {
   connection.release();
-};
-
-module.exports = {
-  getConnection,
-  releaseConnection,
 };
 
 module.exports = {
